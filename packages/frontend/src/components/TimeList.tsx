f669f5d71e802efccbf2dbd1491cdb2fe74842a9
Ulrik Strid <ulrik.strid@outlook.com>
2017-07-27 09:59:22 +0200

    Try antd for time list

--- packages/frontend/src/components/TimeList.tsx
+++ packages/frontend/src/components/TimeList.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import * as moment from "moment";
 
+import { TimePicker, Table } from "antd";
+import { ColumnProps } from "antd/lib/table/Column";
+
 import { Task } from "../models/Task";
 import { TimeEntry, timeToNumber } from "../models/TimeEntry";
 import { TimeEntryFilter } from "../state/reducers/entries";
@@ -12,42 +15,64 @@ interface Props {
   getTimeEntries: () => void;
 }
 
-export const TimeRow = (tasks: Task[]) => (entry: TimeEntry, index: number) => {
-  const entryTask = tasks.find(task => task.id === entry.taskId);
-  const entryMoment = moment(entry.date);
-
-  return (
-    <tr key={entry.id}>
-      <td>
-        {index}
-      </td>
-      <td>
-        {entryMoment.format("MMM Do YY")}
-      </td>
-      <td>
-        <input defaultValue={entry.from} type="time" />
-      </td>
-      <td>
-        <input defaultValue={entry.to} type="time" />
-      </td>
-      <td>
-        {(timeToNumber(entry.to) - timeToNumber(entry.from)) / 60}
-      </td>
-      <td>
-        {entryTask ? entryTask.name : ""}
-      </td>
-    </tr>
-  );
-};
+const tableColumns = (tasks: Task[]): ColumnProps<TimeEntry>[] => [
+  {
+    title: "Task",
+    dataIndex: "taskId",
+    key: "task",
+    render: (taskId, entry) => {
+      const entryTask = tasks.find(task => task.id === entry.taskId);
+      return (
+        <span>
+          {entryTask ? entryTask.name : ""}
+        </span>
+      );
+    }
+  },
+  {
+    title: "Date",
+    dataIndex: "date",
+    key: "date",
+    render: (_, entry) => {
+      return (
+        <span>
+          {entry.date.format("MMM Do -YY")}
+        </span>
+      );
+    }
+  },
+  {
+    title: "From",
+    dataIndex: "from",
+    key: "from",
+    render: from => {
+      return <TimePicker defaultValue={moment(from, "HH:mm")} />;
+    }
+  },
+  {
+    title: "To",
+    dataIndex: "to",
+    key: "to",
+    render: to => {
+      return <TimePicker defaultValue={moment(to, "HH:mm")} />;
+    }
+  },
+  {
+    title: "Total",
+    dataIndex: "total",
+    key: "total",
+    render: (_, entry) => {
+      return (
+        <span>
+          {(timeToNumber(entry.to) - timeToNumber(entry.from)) / 60} h
+        </span>
+      );
+    }
+  }
+];
 
 export default class TimeList extends React.PureComponent<Props, void> {
   render() {
-    console.log(
-      this.props.filter.start,
-      this.props.filter.end,
-      this.props.filter.end.diff(this.props.filter.start, "day")
-    );
-
     const sortedEntries = this.props.timeEntries
       .filter(entry => {
         return (
@@ -58,21 +83,10 @@ export default class TimeList extends React.PureComponent<Props, void> {
       .sort((a, b) => a.date.diff(b.date));
 
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Date</th>
-            <th>From</th>
-            <th>To</th>
-            <th>Total</th>
-            <th>Task</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sortedEntries.map(TimeRow(this.props.tasks))}
-        </tbody>
-      </table>
+      <Table
+        dataSource={sortedEntries}
+        columns={tableColumns(this.props.tasks)}
+      />
     );
   }
 }
